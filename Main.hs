505a4454f6a6898f031b9a29e48b0b9eacbdbe2e
Victor Denisov <denisovenator@gmail.com>
2012-10-10 21:59:00 +0400

    Openjdb is refactored to use Jdi. Cabal file is introduced.

--- Main.hs
+++ Main.hs
@@ -14,8 +14,7 @@ import Data.Binary.Get (runGet, Get)
 import Data.Binary.Put (runPut)
 import Data.Binary (get)
 
-import Jdwp.Protocol
-import Jdwp.Configuration
+import Jdi
 
 openConnection :: String -> PortID -> IO Handle
 openConnection host port = do
@@ -59,134 +58,53 @@ main = do
     args <- getArgs
     let (opts, unparsed, errors) = getOpt Permute options args
     if not $ null errors
-    then putStr $ cmdArgsErrMsg errors
-    else if Version `elem` opts
-         then putStrLn "1.0"
-         else if ((isPort `any` opts) && (isHost `any` opts))
-              then bracket (openConnection (getHost opts) (getPort opts)) hClose mainLoop
-              else putStrLn "Host and port arguments are required"
+        then putStr $ cmdArgsErrMsg errors
+        else if Version `elem` opts
+                 then putStrLn "1.0"
+                 else if ((isPort `any` opts) && (isHost `any` opts))
+                          then  runInputT defaultSettings $ runVirtualMachine (getHost opts) (getPort opts) eventLoop
+                          else putStrLn "Host and port arguments are required"
 
-mainLoop :: Handle -> IO ()
-mainLoop h = do
-    handshake h
-    packet <- waitVmStartEvent h
-    putStrLn $ show packet
-    runInputT defaultSettings $ evalConfT (initialSetup h >> loop) initConf
-    where 
-        loop :: ConfT (InputT IO) ()
-        loop = do
-            minput <- lift $ getInputLine "(jdb) "
-            case minput of
-                Nothing -> return ()
-                Just "quit" -> return ()
-                Just input -> do
-                    cntr <- getPacketIdCounter
-                    incPacketIdCounter
-                    processCommand h cntr input
-                    loop
+eventLoop :: VirtualMachine (InputT IO) ()
+eventLoop = do
+    event <- removeEvent
+    liftIO $ putStrLn $ show event
+    commandLoop
+    eventLoop
 
-handshake :: Handle -> IO ()
-handshake h = do
-    putStrLn "Connected. Initiating handshake..."
-    hPutStr h "JDWP-Handshake"
-    hFlush h
-    value <- B.hGet h 14
-    if value == (B8.pack "JDWP-Handshake")
-    then putStrLn "Handshake successful."
-    else putStrLn "Handshake FAILED!"
+commandLoop :: VirtualMachine (InputT IO) ()
+commandLoop = do
+    minput <- (lift . lift) $ getInputLine "(jdb) "
+    case minput of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just "resume" -> do
+            processCommand "resume"
+            return ()
+        Just input -> do
+            processCommand input
+            commandLoop
 
-initialSetup :: Handle -> ConfT (InputT IO) ()
-initialSetup h = do
-    liftIO $ putStrLn "Sending id sizes request..."
-    cntr <- getPacketIdCounter
-    incPacketIdCounter
-    processCommand h cntr "idsizes"
-
-processCommand :: Handle -> PacketId -> String -> ConfT (InputT IO) ()
-processCommand h cntr "version" = do
-    liftIO $ sendPacket h $ versionCommand cntr
-    liftIO $ putStrLn "version request sent"
-    idsizes <- getIdSizes
-    p <- liftIO $ waitReply h idsizes $ \_ -> parseVersionReply idsizes
+processCommand :: String -> VirtualMachine (InputT IO) ()
+processCommand "version" = do
+    p <- version
     liftIO $ putStrLn $ show p
 
-processCommand h cntr "resume" = do
-    liftIO $ sendPacket h $ resumeVmCommand cntr
-    idsizes <- getIdSizes
-    r <- liftIO $ waitReply h idsizes $ \_ -> parseEmptyData idsizes
-    liftIO $ putStrLn $ show r
-    e <- liftIO $ waitEvent h idsizes
-    liftIO $ putStrLn $ show e
+processCommand "resume" = do
+    resume
 
-processCommand h cntr "idsizes" = do
-    liftIO $ sendPacket h $ idSizesCommand cntr
-    idsizes <- getIdSizes
-    r <- liftIO $ waitReply h idsizes $ \_ -> parseIdSizesReply idsizes
-    liftIO $ putStrLn $ show r
-    setIdSizes $ idSizes $ dat r
-
-processCommand h cntr "show idsizes" = do
+{-
+processCommand "show idsizes" = do
     is <- getIdSizes
     liftIO $ putStrLn $ show is
 
-processCommand h cntr "set" = do
+processCommand "set" = do
     liftIO $ sendPacket h $ eventSetRequest cntr ClassPrepare All
     idsizes <- getIdSizes
     r <- liftIO $ waitReply h idsizes $ \_ -> parseEventSetRequestReply idsizes
     liftIO $ putStrLn $ show r
+    -}
 
-processCommand _ _ cmd = liftIO $
+processCommand cmd = liftIO $
     putStrLn ("Unknown command sequence: " ++ cmd)
 
-receivePacket :: Handle -> IdSizes -> ReplyDataParser -> IO Packet
-receivePacket h idsizes f = do
-    inputAvailable <- hWaitForInput h (-1)
-    if inputAvailable
-    then do putStrLn "Receiving a packet..."
-            lengthString <- B.hGet h 4
-            let length = (fromIntegral $ runGet (parseInt) lengthString) - 4
-            reminder <- B.hGet h length
-            let p = runGet (parsePacket idsizes f) (lengthString `B.append` reminder)
-            return p
-    else error "No input available where expected"
-
-waitReply :: Handle -> IdSizes -> ReplyDataParser -> IO Packet
-waitReply h idsizes f = do
-    packet <- receivePacket h idsizes f
-    case packet of
-        CommandPacket _ _ _ _ _ _ -> error "reply expected, but command received"
-        {- Normally here some queue should be implemented, but currectly for brevity
-         - we assume that we never get event before reply.
-         -}
-        ReplyPacket _ _ _ _ _ -> return packet
-
-waitEvent :: Handle -> IdSizes -> IO Packet
-waitEvent h idsizes = do
-    packet <- receivePacket h idsizes $ \_ -> error "ReplyDataParser is invoked where only command parsing is expected"
-    case packet of
-        CommandPacket _ _ _ _ _ _ -> return packet
-        ReplyPacket _ _ _ _ _ -> error "CommandPacket is expected, but reply packet received"
-        
--- When we parse this event we don't have information about size of threadId.
--- We use the fact that threadId is the last field in the event and we can determine its size
--- as request_length - length_of_fields_before_threadId.
--- for current version of JDWP length_of_fields_before_threadIs is 21.
-waitVmStartEvent :: Handle -> IO Packet
-waitVmStartEvent h = do
-    inputAvailable <- hWaitForInput h (-1)
-    if inputAvailable
-    then do putStrLn "Waiting for VmStartEvent"
-            lengthString <- B.hGet h 4
-            let length = (fromIntegral $ runGet (parseInt) lengthString) - 4
-            reminder <- B.hGet h length
-            let threadIdSize = fromIntegral $ (length + 4) - 21
-            let replyParser = \_ -> error "ReplyDataParser is not expected to be invoked."
-            let p = runGet (parsePacket (IdSizes 0 0 threadIdSize 0 0) replyParser) (lengthString `B.append` reminder)
-            return p
-    else error "No input available where expected"
-    
-
-sendPacket :: Handle -> Packet -> IO ()
-sendPacket h p = do
-    B.hPut h $ runPut $ putPacket p
-    hFlush h
--- runTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -f Jdwp/ConfigurationTest
-rm -f Jdwp/ProtocolTest
-ghc Jdwp/ConfigurationTest.hs
-ghc Jdwp/ProtocolTest.hs
-./Jdwp/ConfigurationTest
-./Jdwp/ProtocolTest
